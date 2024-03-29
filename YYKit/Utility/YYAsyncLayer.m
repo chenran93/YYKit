217aa20891cd01a35c75944d531824ed70e170b7
zhaoyong <zhaoyong57@midea.com>
2024-03-29 19:07:11 +0800

    feature:   适配Xcode15, iOS17, 赋值 YYLabel 的 attributedString，当初始化 NSAttributedString时传入的 string 为 nil，app 执行时会崩溃在UIGraphicsBeginImageContextWithOptions(self.bounds.size, self.opaque, self.contentsScale);

--- YYKit/Utility/YYAsyncLayer.m
+++ YYKit/Utility/YYAsyncLayer.m
@@ -190,6 +190,22 @@ static dispatch_queue_t YYAsyncLayerGetReleaseQueue() {
     } else {
         [_sentinel increase];
         if (task.willDisplay) task.willDisplay(self);
+        
+        // 适配iOS17, 赋值 YYLabel 的 attributedString，
+        // 当初始化 NSAttributedString时传入的 string 为 nil，
+        // app 执行时会崩溃在UIGraphicsBeginImageContextWithOptions(self.bounds.size, self.opaque, self.contentsScale);
+        if (self.bounds.size.width < 1 || self.bounds.size.height < 1) {
+            CGImageRef image = (__bridge_retained CGImageRef)(self.contents);
+            self.contents = nil;
+            if (image) {
+                dispatch_async(YYAsyncLayerGetReleaseQueue(), ^{
+                    CFRelease(image);
+                });
+            }
+            if (task.didDisplay) task.didDisplay(self, YES);
+            return;
+        }
+        
         UIGraphicsBeginImageContextWithOptions(self.bounds.size, self.opaque, self.contentsScale);
         CGContextRef context = UIGraphicsGetCurrentContext();
         if (self.opaque && context) {
